6a4e156fd490cbb6a81504b8a1dbe0109494c6f1
SaulPL <100178696+SPLSaul@users.noreply.github.com>
2025-08-18 10:37:27 -0700

    Update VoltageChart.vue
    Cambio a AreaSeries

--- mi-monitor-voltaje/src/components/VoltageChart.vue
+++ mi-monitor-voltaje/src/components/VoltageChart.vue
@@ -96,7 +96,7 @@ export default {
       console.log('Chart instance:', this.chart)
 
 
-      this.lineSeries = this.chart.addSeries(LineSeries,{
+      this.lineSeries = this.chart.addSeries(AreaSeries,{
         color: '#3498db',
         lineWidth: 3,
         crosshairMarkerVisible: true,
